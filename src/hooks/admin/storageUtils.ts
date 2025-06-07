03a95311af27c7d6ca1218b976df382b3aeca646
guizinho191919 <guilherme_allen@hotmail.com>
2025-06-07 12:45:45 -0400

    Remoção de todos os arquivos do projeto

--- src/hooks/admin/storageUtils.ts
+++
@@ -1,9 +0,0 @@
-
-export const saveToStorage = (key: string, data: any) => {
-  localStorage.setItem(key, JSON.stringify(data));
-};
-
-export const loadFromStorage = (key: string) => {
-  const saved = localStorage.getItem(key);
-  return saved ? JSON.parse(saved) : null;
-};
